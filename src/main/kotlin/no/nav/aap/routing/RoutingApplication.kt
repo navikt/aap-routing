18dc52b704d33bf41ae928f61409de6ef3c4ea67
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-01-26 16:15:45 +0100

    spring cfg

--- src/main/kotlin/no/nav/aap/routing/RoutingApplication.kt
+++ src/main/kotlin/no/nav/aap/routing/RoutingApplication.kt
@@ -1,11 +1,16 @@
 package no.nav.aap.routing
 
+import no.nav.boot.conditionals.Cluster.Companion.profiler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
+@EnableKafka
 class RoutingApplication
 
 fun main(args: Array<String>) {
-	runApplication<RoutingApplication>(*args)
+	runApplication<RoutingApplication>(*args) {
+		setAdditionalProfiles(*profiler())
+	}
 }

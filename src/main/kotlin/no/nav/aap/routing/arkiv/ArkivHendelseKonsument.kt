4cc4939b5245ab78e3cc75c120494bc456ad72ed
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-01-29 10:27:44 +0100

    bruk samme konstant

--- src/main/kotlin/no/nav/aap/routing/arkiv/ArkivHendelseKonsument.kt
+++ src/main/kotlin/no/nav/aap/routing/arkiv/ArkivHendelseKonsument.kt
@@ -11,7 +11,7 @@ import org.springframework.messaging.handler.annotation.Payload
 class ArkivHendelseKonsument(private val client: ArkivClient) {
     private val log = getLogger(javaClass)
 
-    @KafkaListener(topics = ["#{joark-no.nav.aap.routing.arkiv.ArkivConfig.hendelser.topic}"], containerFactory = JOARK)
+    @KafkaListener(topics = ["#{'\${joark.hendelser.topic:teamdokumenthandtering.aapen-dok-journalfoering}'}"], containerFactory = JOARK)
     fun listen(@Payload payload: JournalfoeringHendelseRecord)  =
         client.journalpost(payload.journalpostId).also {  // map til domeneobjekt
             log.info("Payload $payload mottatt, respons SAF $it")

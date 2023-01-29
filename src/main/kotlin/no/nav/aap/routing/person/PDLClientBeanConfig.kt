26545c3a0988c0db31907c8eff075b7dc7a27f08
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-01-29 17:41:53 +0100

    pdl

--- src/main/kotlin/no/nav/aap/routing/person/PDLClientBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/routing/person/PDLClientBeanConfig.kt
@@ -11,32 +11,36 @@ import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClient.Builder
 
-@Bean
-@Qualifier(PDL)
-fun pdlWebClient(b: Builder, cfg: PDLConfig, @Qualifier(PDL) pdlClientCredentialFilterFunction: ExchangeFilterFunction) =
-    b.baseUrl("${cfg.baseUri}")
-        .filter(temaFilterFunction())
-        .filter(pdlClientCredentialFilterFunction)
-        .build()
 
-@Bean
-@Qualifier(PDL)
-fun pdlClientCredentialFilterFunction(cfgs: ClientConfigurationProperties, service: OAuth2AccessTokenService) =
-    ExchangeFilterFunction { req, next ->
-        next.exchange(ClientRequest.from(req).header(AUTHORIZATION, service.bearerToken(cfgs.registration[PDL], req.url())).build())
-    }
+@Configuration
+class PDLClientBeanConfig {
+    @Bean
+    @Qualifier(PDL)
+    fun pdlWebClient(b: Builder, cfg: PDLConfig, @Qualifier(PDL) pdlClientCredentialFilterFunction: ExchangeFilterFunction) =
+        b.baseUrl("${cfg.baseUri}")
+            .filter(temaFilterFunction())
+            .filter(pdlClientCredentialFilterFunction)
+            .build()
 
-@Qualifier(PDL)
-@Bean
-fun pdlGraphQLClient(@Qualifier(PDL) client: WebClient, mapper: ObjectMapper) = GraphQLWebClient.newInstance(client, mapper)
+    @Bean
+    @Qualifier(PDL)
+    fun pdlClientCredentialFilterFunction(cfgs: ClientConfigurationProperties, service: OAuth2AccessTokenService) =
+        ExchangeFilterFunction { req, next ->
+            next.exchange(ClientRequest.from(req).header(AUTHORIZATION, service.bearerToken(cfgs.registration[PDL], req.url())).build())
+        }
 
-@Bean
-@ConditionalOnProperty("$PDL.enabled", havingValue = "true")
-fun pdlHealthIndicator(a: PDLWebClientAdapter) = object : AbstractPingableHealthIndicator(a) {}
+    @Qualifier(PDL)
+    @Bean
+    fun pdlGraphQLClient(@Qualifier(PDL) client: WebClient, mapper: ObjectMapper) = GraphQLWebClient.newInstance(client, mapper)
+
+    @Bean
+    @ConditionalOnProperty("$PDL.enabled", havingValue = "true")
+    fun pdlHealthIndicator(a: PDLWebClientAdapter) = object : AbstractPingableHealthIndicator(a) {}
 }

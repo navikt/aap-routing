4695562b4a3b0112a40aa775a8c2f4a0d11b1ba7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-02 09:24:13 +0100

    auth token egen ansatt

--- src/main/kotlin/no/nav/aap/routing/egenansatt/EgenAnsattBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/routing/egenansatt/EgenAnsattBeanConfig.kt
@@ -2,11 +2,19 @@ package no.nav.aap.routing.egenansatt
 
 import no.nav.aap.health.AbstractPingableHealthIndicator
 import no.nav.aap.routing.egenansatt.EgenAnsattConfig.Companion.EGENANSATT
+import no.nav.aap.routing.person.PDLConfig
+import no.nav.aap.routing.person.PDLConfig.Companion
+import no.nav.aap.util.TokenExtensions.bearerToken
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
+import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer.*
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient.Builder
 
 @Configuration
@@ -14,10 +22,18 @@ class EgenAnsattBeanConfig {
 
     @Qualifier(EGENANSATT)
     @Bean
-    fun egenAnsattWebClient(builder: Builder, cfg: EgenAnsattConfig) =
+    fun egenAnsattWebClient(builder: Builder, cfg: EgenAnsattConfig,@Qualifier(EGENANSATT) egenAnsattClientCredentialFilterFunction: ExchangeFilterFunction) =
         builder
             .baseUrl("${cfg.baseUri}")
+            .filter(egenAnsattClientCredentialFilterFunction)
             .build()
+
+    @Bean
+    @Qualifier(EGENANSATT)
+    fun pdlClientCredentialFilterFunction(cfgs: ClientConfigurationProperties, service: OAuth2AccessTokenService) =
+        ExchangeFilterFunction { req, next ->
+            next.exchange(ClientRequest.from(req).header(HttpHeaders.AUTHORIZATION, service.bearerToken(cfgs.registration[EGENANSATT], req.url())).build())
+        }
 
     @Bean
     @ConditionalOnProperty("$EGENANSATT.enabled", havingValue = "true")

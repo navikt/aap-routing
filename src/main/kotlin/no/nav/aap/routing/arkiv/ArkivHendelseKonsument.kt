30f1f5be84973f71e3020d42efd5f9485797a926
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-06 09:56:31 +0100

    fordeling config

--- src/main/kotlin/no/nav/aap/routing/arkiv/ArkivHendelseKonsument.kt
+++ src/main/kotlin/no/nav/aap/routing/arkiv/ArkivHendelseKonsument.kt
@@ -1,5 +1,9 @@
 package no.nav.aap.routing.arkiv
 
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotEmpty
+import no.nav.aap.api.felles.SkjemaType
+import no.nav.aap.api.felles.SkjemaType.*
 import no.nav.aap.api.felles.error.IntegrationException
 import no.nav.aap.routing.arkiv.JournalpostDTO.JournalStatus.MOTTATT
 import no.nav.aap.routing.egenansatt.EgenAnsattClient
@@ -10,6 +14,8 @@ import no.nav.aap.util.Constants.JOARK
 import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.boot.conditionals.ConditionalOnGCP
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
+import no.nav.security.token.support.client.core.ClientProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
@@ -24,16 +30,24 @@ class ArkivHendelseKonsument(private val fordeler: Fordeler) {
 @Component
 class Fordeler(private val integrator: Integrator) {
     private val log = getLogger(javaClass)
-    fun fordel(id: Long){
-        integrator.slåOpp(id).also {
-            if (MOTTATT == it.journalpost.journalstatus) {
-                log.info("Håndterer $it (snart)")
+    fun fordel(id: Long) =
+        with(integrator.slåOpp(id)) {
+            if (MOTTATT == journalpost.journalstatus  && journalpost.dokumenter.any { it.brevkode in listOf(
+                        STANDARD.kode,
+                        STANDARD_ETTERSENDING.kode) }) {
+                log.info("Håndterer $this (snart)")  // TODO gjør konfigurerbart pr tema og brevkode
             }
             else  {
-                log.info("Ignorerer ${it.journalpost.journalstatus}")
+                log.info("Ignorerer ${journalpost.journalstatus}")
             }
         }
-    }
+}
+
+@ConfigurationProperties("fordeling")
+data class FordelingConfigurationProperties(val registration: @NotEmpty Map<String, FordelingProperties>) {
+
+    data class FordelingProperties(val brevkoder: List<String>)
+
 }
 @Component
 class Integrator(private val integrasjoner: Integrasjoner) {

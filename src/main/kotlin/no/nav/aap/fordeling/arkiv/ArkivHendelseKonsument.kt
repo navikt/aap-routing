ce38300bda7709efd82ee9db104e70633b74c60c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-07 20:47:10 +0100

    any

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivHendelseKonsument.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivHendelseKonsument.kt
@@ -23,11 +23,15 @@ class ArkivHendelseKonsument(private val delegator: DelegerendeFordeler) {
 
 
     @KafkaListener(topics = ["#{'\${joark.hendelser.topic:teamdokumenthandtering.aapen-dok-journalfoering}'}"], containerFactory = JOARK)
-    fun listen(@Payload payload: JournalfoeringHendelseRecord)  = delegator.deleger(payload.journalpostId, payload.temaNytt)
+    fun listen(@Payload payload: JournalfoeringHendelseRecord)  {
+        delegator.deleger(payload.journalpostId, payload.temaNytt)
+    }
 
     //@KafkaListener(topics = ["aap.routing-dlt"], containerFactory = JOARK)
     @DltHandler
-    fun dlt(@Payload payload: JournalfoeringHendelseRecord)  = log.info("OOPS, DEAD LETTER $payload")
+    fun dlt(@Payload payload: JournalfoeringHendelseRecord)   {
+        log.info("OOPS, DEAD LETTER $payload")
+    }
 }
 
 fun FordelingConfigurationProperties.finnFordeler(jp: Journalpost, tema: String, fordelere: List<Fordeler>):Fordeler?  {

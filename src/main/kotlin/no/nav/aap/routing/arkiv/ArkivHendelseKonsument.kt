dd44c74eaa0922695f6867d0f8ac47d8f19204a1
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-07 12:05:04 +0100

    bedre bstraksjoner for fordeling

--- src/main/kotlin/no/nav/aap/routing/arkiv/ArkivHendelseKonsument.kt
+++ src/main/kotlin/no/nav/aap/routing/arkiv/ArkivHendelseKonsument.kt
@@ -1,12 +1,7 @@
 package no.nav.aap.routing.arkiv
 
-import jakarta.validation.Valid
-import jakarta.validation.constraints.NotEmpty
-import no.nav.aap.api.felles.SkjemaType
-import no.nav.aap.api.felles.SkjemaType.*
 import no.nav.aap.api.felles.error.IntegrationException
-import no.nav.aap.routing.arkiv.JournalpostDTO.JournalStatus
-import no.nav.aap.routing.arkiv.JournalpostDTO.JournalStatus.MOTTATT
+import no.nav.aap.routing.arkiv.Fordeler.FordelingResultat
 import no.nav.aap.routing.egenansatt.EgenAnsattClient
 import no.nav.aap.routing.navorganisasjon.NavEnhet
 import no.nav.aap.routing.navorganisasjon.NavOrgClient
@@ -15,54 +10,63 @@ import no.nav.aap.util.Constants.JOARK
 import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.boot.conditionals.ConditionalOnGCP
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
-import no.nav.security.token.support.client.core.ClientProperties
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
 
 @ConditionalOnGCP
-class ArkivHendelseKonsument(private val fordeler: Fordeler) {
+class ArkivHendelseKonsument(private val delegator: RutingDelegator) {
 
     @KafkaListener(topics = ["#{'\${joark.hendelser.topic:teamdokumenthandtering.aapen-dok-journalfoering}'}"], containerFactory = JOARK)
-    fun listen(@Payload payload: JournalfoeringHendelseRecord)  = fordeler.fordel(payload.journalpostId)
+    fun listen(@Payload payload: JournalfoeringHendelseRecord)  = delegator.deleger(payload.journalpostId, payload.temaNytt)
 }
 
 @Component
-class Fordeler(private val integrator: Integrator) {
+class RutingDelegator(private val cfg: FordelingConfigurationProperties, val arkiv: ArkivClient,private val fordeler: List<Fordeler>) {
     private val log = getLogger(javaClass)
-    fun fordel(id: Long) =
-        with(integrator.slåOpp(id)) {
-            if (MOTTATT == journalpost.journalstatus  && journalpost.dokumenter.any { it.brevkode in listOf(
-                        STANDARD.kode,
-                        STANDARD_ETTERSENDING.kode) }) {
-                log.info("Håndterer $this (snart)")  // TODO gjør konfigurerbart pr tema og brevkode
-            }
-            else  {
-                log.info("Ignorerer ${journalpost.journalstatus}")
-            }
-        }
+    fun deleger(id: Long, tema: String) =
+        arkiv.journalpost(id)?.let {jp ->
+            cfg.routing[tema.lowercase()]?.let { c ->
+                if (jp.journalstatus in c.statuser && jp.dokumenter.any { it.brevkode in c.brevkoder})  {
+                    log.info("Fordeler $this (snart)")
+                    fordeler.find { it.mode() == c.mode }
+                        ?.fordel(jp) ?: log.warn("Fant ingen ruter for tema $tema og mode ${c.mode}")
+                }
+                else {
+                    log.info("Journalpost $jp for $tema rutes ikke")
+                }
+            } ?: log.info("Fant ingen konfigurasjon for $tema, kjenner kun til følgende:  ${cfg.routing.keys}")
+        } ?: log.info("Ingen journalpost for id $id")
 }
 
-@ConfigurationProperties("fordeling")
-data class FordelingConfigurationProperties(val routing: @NotEmpty Map<String, FordelingProperties>) {
 
-    data class FordelingProperties(val mode: String, val statuser: List<JournalStatus>,val brevkoder: List<String>)
-
+@Component
+class LegacyAAPFordeler(private val integrator: Integrator) : Fordeler {
+    override fun mode() = "legacy"
+    override fun fordel(journalpost: Journalpost): FordelingResultat {
+           integrator.slåOpp(journalpost)
+        return FordelingResultat("OK")
+    }
 }
+
+
+interface Fordeler {
+    fun mode(): String
+    fun fordel(journalpost: Journalpost) : FordelingResultat
+    data class FordelingResultat(val status: String)
+}
+
 @Component
 class Integrator(private val integrasjoner: Integrasjoner) {
-    fun slåOpp(journalpost: Long) =
+    fun slåOpp(jp: Journalpost) =
         runCatching {
             with(integrasjoner) {
-                arkiv.journalpost(journalpost)?.let { jp ->
                     OppslagResultat(jp, org.navEnhet(pdl.geoTilknytning(jp.fnr), egen.erSkjermet(jp.fnr), pdl.diskresjonskode(jp.fnr)))
                 } ?: throw IntegrationException("Ingen journalpost")
-            }
         }.getOrThrow()
 
     data class OppslagResultat(val journalpost: Journalpost,  val enhet: NavEnhet)
 }
 
 @Component
-data class Integrasjoner(val arkiv: ArkivClient, val pdl: PDLClient, val org: NavOrgClient, val egen: EgenAnsattClient)
+data class Integrasjoner(val pdl: PDLClient, val org: NavOrgClient, val egen: EgenAnsattClient)
---
+++ src/main/kotlin/no/nav/aap/routing/arkiv/FordelingConfigurationProperties.kt
@@ -0,0 +1,15 @@
+package no.nav.aap.routing.arkiv
+
+import jakarta.validation.constraints.NotEmpty
+import no.nav.aap.routing.arkiv.FordelingConfigurationProperties.Companion.FORDELING
+import no.nav.aap.routing.arkiv.JournalpostDTO.JournalStatus
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(FORDELING)
+data class FordelingConfigurationProperties(val routing: @NotEmpty Map<String, FordelingProperties>) {
+    data class FordelingProperties(val mode: String, val statuser: List<JournalStatus>, val brevkoder: List<String>)
+
+    companion object {
+        const val FORDELING = "fordeling"
+    }
+}

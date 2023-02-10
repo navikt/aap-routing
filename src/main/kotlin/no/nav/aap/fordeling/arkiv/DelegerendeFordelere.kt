e963d27fa9e53b9e6bdd27a1a1babd8223095e8c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-10 12:05:39 +0100

    skjelett for manuell

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/DelegerendeFordelere.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/DelegerendeFordelere.kt
@@ -1,14 +1,19 @@
 package no.nav.aap.fordeling.arkiv
 
 import no.nav.aap.fordeling.arkiv.Fordeler.Companion
-import no.nav.aap.fordeling.arkiv.Fordeler.Companion.INGEN_FORDELER
 import no.nav.aap.fordeling.arkiv.Fordeler.FordelingResultat
 import org.springframework.stereotype.Component
 
+@Component
+class DelegerendeFordeler(private val cfg: FordelingConfiguration, private val fordelere: List<Fordeler>) :
+    Fordeler {
+    override fun tema() = fordelere.map(Fordeler::tema).flatten()
+    override fun fordel(jp: Journalpost) = cfg.fordelerFor(jp,fordelere).fordel(jp)
+}
 @Component
 class DelegerendeManuellFordeler(private val fordelere: List<ManuellFordeler>) : ManuellFordeler {
     override fun tema() = fordelere.map(Fordeler::tema).flatten()
     override fun fordel(journalpost: Journalpost): FordelingResultat = fordelere.find { journalpost.tema in it.tema() }?.let {
         it.fordel(journalpost)
-    } ?: INGEN_FORDELER.fordel(journalpost)
+    } ?: Fordeler.INGEN_FORDELER.fordel(journalpost)
 }

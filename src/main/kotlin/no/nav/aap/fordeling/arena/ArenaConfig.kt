4cdb00fc20c87a8e2fb1ed39ef6e5b418be3dc0a
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-13 14:24:20 +0100

    feil config, trenger ikke token

--- src/main/kotlin/no/nav/aap/fordeling/arena/ArenaConfig.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arena/ArenaConfig.kt
@@ -13,14 +13,14 @@ class ArenaConfig(
     @DefaultValue(DEFAULT_PING_PATH) pingPath: String,
     @DefaultValue("true") enabled: Boolean,
     @DefaultValue(SAKER_PATH) val sakerPath: String,
-    @DefaultValue(AKTIV_SAKER_PATH) val aktivSakPath: String,
+    @DefaultValue(AKTIV_SAK_PATH) val aktivSakPath: String,
     @NestedConfigurationProperty private val retryCfg: RetryConfig = DEFAULT,
     baseUri: URI) : AbstractRestConfig(baseUri, pingPath, ARENA, enabled,retryCfg) {
     override fun toString() = "${javaClass.simpleName} [pingPath=$pingPath,enabled=$isEnabled,baseUri=$baseUri]"
 
     companion object {
-        private const val SAKER_PATH = "arena/saker/{}"
-        private const val AKTIV_SAKER_PATH = "arena/haraktivsak/{}"
+        private const val SAKER_PATH = "arena/saker/{fnr}"
+        private const val AKTIV_SAK_PATH = "arena/haraktivsak/{fnr}"
 
 
         private const val DEFAULT_PING_PATH = "actuator/health/liveness"

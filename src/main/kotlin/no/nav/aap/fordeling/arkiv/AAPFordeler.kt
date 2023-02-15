c9f3296ff3c834c42693eee527642572fedae96a
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-15 19:34:29 +0100

    ikke kast på ukjent brevkode

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/AAPFordeler.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/AAPFordeler.kt
@@ -6,33 +6,34 @@ import no.nav.aap.fordeling.navorganisasjon.EnhetsKriteria.Status.AKTIV
 import no.nav.aap.fordeling.navorganisasjon.NavEnhet
 import no.nav.aap.util.Constants.AAP
 import no.nav.aap.util.LoggerUtil
+import no.nav.aap.util.LoggerUtil.getLogger
 import org.springframework.stereotype.Component
 
 @Component
 class AAPFordeler(private val integrasjoner: Integrasjoner,private val manuell: AAPManuellFordeler) : Fordeler {
 
-    private val log = LoggerUtil.getLogger(javaClass)
+    private val log = getLogger(javaClass)
     override fun tema() = listOf(AAP)
     override fun fordel(jp: Journalpost): FordelingResultat {
         val navEnhet = navEnhet(jp).also { log.info("NAV-enhet er $it") }
-        var brevkode = jp.dokumenter.first().brevkode // alltid først
-            when (brevkode)  {
-                STANDARD.kode -> fordelStandard(jp, navEnhet) // 2c
-                STANDARD_ETTERSENDING.kode ->  fordelEttersending(jp, navEnhet) // 2d
-                else ->  log.info("Fordeler ikke brevkode $brevkode")
-            }
-            return FordelingResultat("OK")
+        when (val brevkode = jp.dokumenter.first().brevkode) {
+            STANDARD.kode -> fordelStandard(jp, navEnhet) // 2c
+            STANDARD_ETTERSENDING.kode -> fordelEttersending(jp, navEnhet) // 2d
+            else -> FordelingResultat("Ikke fordelt")
+        }
     }
 
     private fun fordelEttersending(jp: Journalpost, navEnhet: NavEnhet) =
         integrasjoner.arena.hentNyesteAktiveSak().let {
             integrasjoner.arkiv.oppdaterOgFerdigstill(jp, it, navEnhet) // 3a/b
+            FordelingResultat("Ettersending")
         }
 
     private fun fordelStandard(jp: Journalpost, navEnhet: NavEnhet) =
         if (!integrasjoner.arena.harAktivSak(jp)) { // 2c-1
             val sak = integrasjoner.arena.opprettStartVedtak()  // 2c-2
             integrasjoner.arkiv.oppdaterOgFerdigstill(jp, sak, navEnhet) // 3a/b
+            FordelingResultat("OK")
         }
         else {
             manuell.fordel(jp)

558314111165d58a477e22ab55575b33034a1ba2
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-20 18:39:59 +0100

    dev controller

--- src/main/kotlin/no/nav/aap/fordeling/config/GlobalBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/fordeling/config/GlobalBeanConfig.kt
@@ -26,12 +26,26 @@ import reactor.netty.http.client.HttpClient
 import reactor.util.retry.Retry.fixedDelay
 import reactor.netty.transport.logging.AdvancedByteBufFormat.TEXTUAL
 import io.netty.handler.logging.LogLevel.TRACE
+import io.swagger.v3.oas.annotations.info.License
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
+import org.springframework.boot.info.BuildProperties
 
 @Configuration
 class GlobalBeanConfig(@Value("\${spring.application.name}") private val applicationName: String)  {
 
+    @Bean
+    fun swagger(p: BuildProperties): OpenAPI {
+        return OpenAPI()
+            .info(Info()
+                .title("AAP fss proxy")
+                .description("Proxy mot tjenester som ikke støtter AAD/TokenX")
+                .version(p.version)
+                .license(License()
+                    .name("MIT")
+                    .url("https://nav.no"))
+                 )
+    }
     @Bean
     fun webClientCustomizer(client: HttpClient, registry: MeterRegistry) =
         WebClientCustomizer { b ->

78c092e7fe8fca6e9895b0860ab061b9f7e1b917
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-20 10:31:41 +0100

    opprett sak

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivClient.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivClient.kt
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Component
 @Component
 class ArkivClient(private val a: ArkivWebClientAdapter) {
     fun journalpost(id: Long)  = a.journalpost(id)
-    fun oppdaterOgFerdigstill(jp: Journalpost, sak: Unit,navEnhet: NavEnhet) = Unit // TODO
+    fun oppdaterOgFerdigstill(jp: Journalpost, sak: Any,navEnhet: NavEnhet) = Unit // TODO
 }

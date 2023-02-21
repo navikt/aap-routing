a9b351bc41d04d9f01e3c522379316a8f476e3bb
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-21 18:47:38 +0100

    chore

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/AAPFordeler.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/AAPFordeler.kt
@@ -18,14 +18,14 @@ class AAPFordeler(private val integrasjoner: Integrasjoner,private val manuell: 
     override fun fordel(journalpost: Journalpost) =
         runCatching {
             when (val brevkode = journalpost.dokumenter.first().brevkode) {
-                STANDARD.kode -> fordelStandard(journalpost) // 2c
-                STANDARD_ETTERSENDING.kode -> fordelEttersending(journalpost) // 2d
+                STANDARD.kode -> fordelStandard(journalpost)
+                STANDARD_ETTERSENDING.kode -> fordelEttersending(journalpost)
                 else -> FordelingResultat(msg="$brevkode ikke konfigurert for fordeling for ${tema()}").also {
                     log.info("$brevkode ikke konfigurert for fordeling for ${tema()}")
                 }
             }
         }.getOrElse {
-            log.warn("Noe gikk galt under fordeling, går til manuell",it)
+            log.warn("Noe gikk galt under fordeling, prøver manuell",it)
             manuell.fordel(journalpost)
         }
 
@@ -33,10 +33,10 @@ class AAPFordeler(private val integrasjoner: Integrasjoner,private val manuell: 
 
     private fun fordelStandard(journalpost: Journalpost) =
         with(integrasjoner) {
-            if (!arena.harAktivArenaSak(journalpost)) { // 2c-1
+            if (!arena.harAktivArenaSak(journalpost)) {
                 arena.opprettArenaOppgave(journalpost, navEnhet(journalpost)).run {
-                    arkiv.oppdaterOgFerdigstillJournalpost(journalpost, arenaSakId) // 3a/b
-                }  // 2c-2
+                    arkiv.oppdaterOgFerdigstillJournalpost(journalpost, arenaSakId)
+                }
             }
             else {
                 manuell.fordel(journalpost)

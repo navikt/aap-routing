e897c374b5c9129b2167166c8aef815fc8633129
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-22 16:58:44 +0100

    identer

--- src/main/kotlin/no/nav/aap/fordeling/person/PDLWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/fordeling/person/PDLWebClientAdapter.kt
@@ -26,7 +26,7 @@ class PDLWebClientAdapter(@Qualifier(PDL) val client: WebClient, @Qualifier(PDL)
 
     fun diskresjonskode(fnr: Fødselsnummer) = query<PDLAdressebeskyttelse>(graphQL,BESKYTTELSE_QUERY, fnr.asIdent())?.tilDiskresjonskode() ?: ANY
 
-    fun aktørId(fnr: Fødselsnummer) = query<Any>(graphQL, IDENT_QUERY, fnr.asIdent())//?.first()?.get(IDENT)?.let(::AktørId)
+    fun aktørId(fnr: Fødselsnummer) = query<List<Any>>(graphQL, IDENT_QUERY, fnr.asIdent())//?.first()?.get(IDENT)?.let(::AktørId)
 
     fun geoTilknytning(fnr: Fødselsnummer) = query<PDLGeoTilknytning>(graphQL, GT_QUERY, fnr.asIdent())?.gt()
     private fun Fødselsnummer.asIdent() = mapOf(IDENT to fnr)

bb5ddfbe2c77dacd789037201295118c3425854a
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-27 20:23:02 +0100

    kun MOTTATT er interessant

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivBeanConfig.kt
@@ -6,6 +6,8 @@ import kotlin.annotation.AnnotationRetention.RUNTIME
 import kotlin.annotation.AnnotationTarget.ANNOTATION_CLASS
 import kotlin.annotation.AnnotationTarget.CLASS
 import no.nav.aap.fordeling.arkiv.ArkivConfig.Companion.DOKARKIV
+import no.nav.aap.fordeling.arkiv.JournalpostDTO.JournalStatus
+import no.nav.aap.fordeling.arkiv.JournalpostDTO.JournalStatus.*
 import no.nav.aap.fordeling.config.GlobalBeanConfig.Companion.clientCredentialFlow
 import no.nav.aap.health.AbstractPingableHealthIndicator
 import no.nav.aap.util.Constants.JOARK
@@ -13,23 +15,25 @@ import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerConfig.*
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.AliasFor
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.listener.DefaultErrorHandler
+import org.springframework.kafka.listener.adapter.RecordFilterStrategy
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer.*
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.util.backoff.FixedBackOff
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClient.Builder
+
+
 
 @Configuration
 @EnableScheduling
@@ -63,10 +67,14 @@ class ArkivBeanConfig {
         ConcurrentKafkaListenerContainerFactory<String, JournalfoeringHendelseRecord>().apply {
             consumerFactory = DefaultKafkaConsumerFactory(p.buildConsumerProperties().apply {
                 setCommonErrorHandler(DefaultErrorHandler(FixedBackOff(1000L, 5L)).apply {
-                    setRecordFilterStrategy { it.value().temaNytt.lowercase() !in props.routing.keys } })
+                    setRecordFilterStrategy {
+                        with (it.value()) {
+                            !(temaNytt.lowercase() in props.routing.keys && journalpostStatus == MOTTATT.name)
+                        }
+                    }
+                })
             })
         }
-
     @Bean
     @ConditionalOnProperty("${JOARK}.enabled", havingValue = "true")
     fun arkivHealthIndicator(adapter: ArkivWebClientAdapter) = object : AbstractPingableHealthIndicator(adapter) {}

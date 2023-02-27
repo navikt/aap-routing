e2bccb07509a3cdb9df7584915c06ad5f24c4b9b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-27 12:52:03 +0100

    ferdigstill

--- src/main/kotlin/no/nav/aap/fordeling/DevController.kt
+++ src/main/kotlin/no/nav/aap/fordeling/DevController.kt
@@ -2,6 +2,7 @@ package no.nav.aap.fordeling
 
 import no.nav.aap.fordeling.arkiv.ArkivWebClientAdapter
 import no.nav.aap.fordeling.arkiv.JournalpostDTO.OppdaterForespørsel
+import no.nav.aap.util.LoggerUtil
 import no.nav.security.token.support.spring.UnprotectedRestController
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @UnprotectedRestController(value = ["/dev"])
 class DevController(private val arkiv: ArkivWebClientAdapter, private val integrassjoner: Integrasjoner) {
+
+    private val log = LoggerUtil.getLogger(javaClass)
     @PostMapping("oppdaterogferdigstill")
     fun oppdaterOgFerdigstill(@RequestBody  data: OppdaterForespørsel, @RequestParam journalpostId: String) = arkiv.oppdaterOgFerdigstillJournalpost(journalpostId,data)
 
@@ -17,7 +20,14 @@ class DevController(private val arkiv: ArkivWebClientAdapter, private val integr
     fun oppdater( @RequestParam journalpostId: String,@RequestBody  data: OppdaterForespørsel) = arkiv.oppdaterJournalpost(journalpostId,data)
 
     @PostMapping("ferdigstill")
-    fun ferdigstill( @RequestParam journalpostId: String) = arkiv.ferdigstillJournalpost(journalpostId)
+    fun ferdigstill( @RequestParam journalpostId: String): Any? {
+        kotlin.runCatching {
+            log.info("Ferdigstilling journalpost $journalpostId")
+            return arkiv.ferdigstillJournalpost(journalpostId)
+        }.getOrElse{
+            log.info("Ferdigstilling feilet")
+            throw it
+        }
 
     @GetMapping("haroppgave")
     fun harOppgave(@RequestParam journalpostId: String) = integrassjoner.oppgave.harOppgave(journalpostId)

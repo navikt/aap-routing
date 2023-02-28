e3f0ee8cea2f34816f8d93d0bffe3e414e5377b1
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-28 20:20:10 +0100

    retry testing

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivBeanConfig.kt
@@ -2,12 +2,13 @@ package no.nav.aap.fordeling.arkiv
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import graphql.kickstart.spring.webclient.boot.GraphQLWebClient
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import java.util.*
 import no.nav.aap.fordeling.arkiv.ArkivConfig.Companion.DOKARKIV
 import no.nav.aap.fordeling.arkiv.JournalpostDTO.JournalStatus.MOTTATT
 import no.nav.aap.fordeling.config.GlobalBeanConfig.Companion.clientCredentialFlow
 import no.nav.aap.health.AbstractPingableHealthIndicator
 import no.nav.aap.util.Constants.JOARK
-import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
@@ -18,10 +19,13 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.config.KafkaListenerEndpoint
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.listener.DefaultErrorHandler
 import org.springframework.kafka.retrytopic.DestinationTopic.Properties
+import org.springframework.kafka.retrytopic.RetryTopicConfiguration
+import org.springframework.kafka.retrytopic.RetryTopicConfigurationBuilder
 import org.springframework.kafka.retrytopic.RetryTopicNamesProviderFactory
 import org.springframework.kafka.retrytopic.RetryTopicNamesProviderFactory.RetryTopicNamesProvider
 import org.springframework.kafka.retrytopic.SuffixingRetryTopicNamesProviderFactory.SuffixingRetryTopicNamesProvider
@@ -79,28 +83,51 @@ class ArkivBeanConfig {
                 })
             })
         }
+
+    @Bean
+    fun retryingKafkaOperations(p: KafkaProperties) =
+        KafkaTemplate(DefaultKafkaProducerFactory<String, JournalfoeringHendelseRecord>(p.buildProducerProperties()))
     @Bean
     @ConditionalOnProperty("${JOARK}.enabled", havingValue = "true")
     fun arkivHealthIndicator(adapter: ArkivWebClientAdapter) = object : AbstractPingableHealthIndicator(adapter) {}
 
     @Component
-    class CustomRetryTopicNamesProviderFactory : RetryTopicNamesProviderFactory {
+    class TopicNamingProviderFactory : RetryTopicNamesProviderFactory {
 
-        val log = getLogger(javaClass)
-
-        override fun createRetryTopicNamesProvider(properties: Properties): RetryTopicNamesProvider {
-            return MyTopicNameProvider()
+        override fun createRetryTopicNamesProvider(p: Properties): RetryTopicNamesProvider {
+            if (p.isMainEndpoint) {
+                return SuffixingRetryTopicNamesProvider(p)
+            }
+            if (p.isDltTopic) {
+                return object : SuffixingRetryTopicNamesProvider(p) {
+                    override fun getTopicName(topic: String): String {
+                        return "aap.routing.dlt"
+                    }
+                }
+            }
+            return object : SuffixingRetryTopicNamesProvider(p) {
+                override fun getTopicName(topic: String): String {
+                    return "aap.routing.retry"
+                }
+            }
         }
     }
-    class MyTopicNameProvider : RetryTopicNamesProvider {
-        override fun getEndpointId(endpoint: KafkaListenerEndpoint) = endpoint.id!!
 
-        override fun getGroupId(endpoint: KafkaListenerEndpoint) = endpoint.groupId!!
-
-        override fun getClientIdPrefix(endpoint: KafkaListenerEndpoint) = endpoint.clientIdPrefix!!
-
-        override fun getGroup(endpoint: KafkaListenerEndpoint) = endpoint.group!!
-
-        override fun getTopicName(endpoint: String) = "aap.jalla"
+    @Bean
+    fun retryTopicConfig(props: KafkaProperties, factory: ConcurrentKafkaListenerContainerFactory<String, JournalfoeringHendelseRecord>, template: KafkaTemplate<String, JournalfoeringHendelseRecord>): RetryTopicConfiguration?{
+        val cfg = props.retry.topic
+        val backoff = FixedBackOff(cfg.delay.toMillis(), cfg.attempts.toLong())
+        return RetryTopicConfigurationBuilder
+            .newInstance()
+           // .autoCreateTopics(false,2,2)
+            .doNotAutoCreateRetryTopics()
+            .fixedBackOff(backoff.interval)
+            .maxAttempts(Math.toIntExact(backoff.maxAttempts))
+            .useSingleTopicForFixedDelays()
+           // .retryTopicSuffix(".retry")
+            //.dltSuffix(".dlt")
+            .doNotRetryOnDltFailure()
+            .listenerFactory(factory)
+            .create(template)
     }
 }

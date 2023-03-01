38e085210e991a29255b132964e14c50665492af
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-01 19:30:49 +0100

    topic

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivHendelseKonsument.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivHendelseKonsument.kt
@@ -10,19 +10,29 @@ import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.annotation.RetryableTopic
 import org.springframework.kafka.retrytopic.FixedDelayStrategy
 import org.springframework.kafka.retrytopic.FixedDelayStrategy.*
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.kafka.support.KafkaHeaders.*
+import org.springframework.messaging.handler.annotation.Header
 import org.springframework.retry.annotation.Backoff
 
 @ConditionalOnGCP
 class ArkivHendelseKonsument(private val fordeler: DelegerendeFordeler, val integrasjoner: Integrasjoner) {
 
+   var counter = 0
+
     val log = getLogger(javaClass)
+
 
     @KafkaListener(topics = ["#{'\${joark.hendelser.topic:teamdokumenthandtering.aapen-dok-journalfoering}'}"], containerFactory = JOARK)
     @RetryableTopic(attempts = "3", backoff = Backoff(delay = 1000),fixedDelayTopicStrategy = SINGLE_TOPIC, autoCreateTopics = "false")
-    fun listen(payload: JournalfoeringHendelseRecord)  {
+    fun listen(payload: JournalfoeringHendelseRecord, @Header(RECEIVED_TOPIC) topic: String)  {
         runCatching {
-            log.trace("Mottok hendelse $payload")
-            throw IllegalArgumentException("OOPS TEST")
+            log.trace("Mottok hendelse $payload på topic $topic")
+            if (counter++ < 3) {
+                log.info("Kaster exception")
+                throw IllegalArgumentException("OOPS TEST")
+            }
+            log.info("Fordeler")
             with(integrasjoner) {
                 arkiv.hentJournalpost(payload.journalpostId)?.let {
                     fordeler.fordel(it,navEnhet(it))
@@ -32,7 +42,7 @@ class ArkivHendelseKonsument(private val fordeler: DelegerendeFordeler, val inte
     }
 
    @DltHandler
-    fun dlt(payload: JournalfoeringHendelseRecord)  {
-            log.info("Mottok hendelse retry $payload")
+    fun dlt(payload: JournalfoeringHendelseRecord,@Header(RECEIVED_TOPIC) topic: String)  {
+            log.info("Mottok DLT hendelse på $topic  $payload")
     }
 }

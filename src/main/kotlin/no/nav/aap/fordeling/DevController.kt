7c7ff052e0c74c0462a0e21cb5ace9829b3b454e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-02 10:42:33 +0100

    fordel i dev

--- src/main/kotlin/no/nav/aap/fordeling/DevController.kt
+++ src/main/kotlin/no/nav/aap/fordeling/DevController.kt
@@ -3,9 +3,13 @@ package no.nav.aap.fordeling
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.fordeling.arena.ArenaDTOs.ArenaOpprettOppgaveData
 import no.nav.aap.fordeling.arena.ArenaWebClientAdapter
+import no.nav.aap.fordeling.arkiv.AAPFordeler
 import no.nav.aap.fordeling.arkiv.ArkivWebClientAdapter
+import no.nav.aap.fordeling.arkiv.Journalpost
 import no.nav.aap.fordeling.arkiv.JournalpostDTO.OppdaterForespørsel
 import no.nav.aap.fordeling.egenansatt.EgenAnsattClient
+import no.nav.aap.fordeling.navorganisasjon.EnhetsKriteria.NavEnhet
+import no.nav.aap.fordeling.navorganisasjon.EnhetsKriteria.Status.AKTIV
 import no.nav.aap.fordeling.navorganisasjon.NavOrgWebClientAdapter
 import no.nav.aap.fordeling.oppgave.OppgaveClient
 import no.nav.aap.fordeling.person.PDLWebClientAdapter
@@ -18,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 
 @UnprotectedRestController(value = ["/dev"])
-class DevController(private val pdl: PDLWebClientAdapter, private val egen: EgenAnsattClient, private val arkiv: ArkivWebClientAdapter, private val oppgave: OppgaveClient, private val arena: ArenaWebClientAdapter, private val org: NavOrgWebClientAdapter) {
+class DevController(private val fordeler: AAPFordeler,private val pdl: PDLWebClientAdapter, private val egen: EgenAnsattClient, private val arkiv: ArkivWebClientAdapter, private val oppgave: OppgaveClient, private val arena: ArenaWebClientAdapter, private val org: NavOrgWebClientAdapter) {
 
     private val log = getLogger(javaClass)
     @PostMapping("oppdaterogferdigstilljournalpost")
@@ -30,6 +34,10 @@ class DevController(private val pdl: PDLWebClientAdapter, private val egen: Egen
     fun ferdigstillJournalpost( @RequestParam journalpostId: String) =
          arkiv.ferdigstillJournalpost(journalpostId)
 
+
+    @PostMapping("fordel")
+    fun fordelSøknad(@RequestBody  journalpost: Journalpost,@RequestParam enhetNr: String) =
+        fordeler.fordel(journalpost, NavEnhet(enhetNr,AKTIV))
     @GetMapping("hargosysoppgave")
     fun gosysHarOppgave(@RequestParam journalpostId: String) = oppgave.harOppgave(journalpostId)
 

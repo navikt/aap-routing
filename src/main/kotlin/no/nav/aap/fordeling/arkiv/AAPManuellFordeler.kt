e6a0f8dab8dd06b335a5db3bdce187028f7cd95f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-04 00:13:05 +0100

    chore

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/AAPManuellFordeler.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/AAPManuellFordeler.kt
@@ -6,34 +6,35 @@ import no.nav.aap.fordeling.arkiv.Fordeler.FordelingResultat.FordelingType.INGEN
 import no.nav.aap.fordeling.arkiv.Fordeler.FordelingResultat.FordelingType.MANUELL_FORDELING
 import no.nav.aap.fordeling.arkiv.Fordeler.FordelingResultat.FordelingType.MANUELL_JOURNALFØRING
 import no.nav.aap.fordeling.navorganisasjon.EnhetsKriteria.NAVEnhet
+import no.nav.aap.fordeling.oppgave.OppgaveClient
 import no.nav.aap.util.Constants.AAP
 import no.nav.aap.util.LoggerUtil.getLogger
 import org.springframework.stereotype.Component
 
 @Component
-class AAPManuellFordeler(private val integrasjoner: Integrasjoner) : ManuellFordeler {
+class AAPManuellFordeler(private val oppgave: OppgaveClient) : ManuellFordeler {
     val log = getLogger(AAPManuellFordeler::class.java)
 
     override fun tema() = listOf(AAP)
 
     override fun fordel(jp: Journalpost, enhet: NAVEnhet) =
-        with(integrasjoner)  {
+        with(jp)  {
             if (oppgave.harOppgave(jp.journalpostId)) {
-                FordelingResultat(jp.journalpostId,"Har allerede journalføringsoppgave", INGEN)
+                FordelingResultat(journalpostId,"Det finnes allerede journalføringsoppgave for journalpost", INGEN)
             }
             else {
                 runCatching {
-                    log.info("Oppretter manuell journalføringsoppgave for journalpost ${jp.journalpostId}")
+                    log.info("Oppretter manuell journalføringsoppgave for journalpost $journalpostId")
                     oppgave.opprettManuellJournalføringOppgave(jp,enhet)
-                    FordelingResultat(jp.journalpostId,"journalføringsoppgave opprettet",MANUELL_JOURNALFØRING)
+                    FordelingResultat(journalpostId,"Journalføringsoppgave opprettet",MANUELL_JOURNALFØRING)
                 }.getOrElse {
                     runCatching {
-                        log.warn("Opprettelse av manuell journalføringsopgave for journalpost ${jp.journalpostId} feilet, oppretter fordelingsoppgave", it)
+                        log.warn("Feil ved opprettelse av manuell journalføringsopgave for journalpost $journalpostId, oppretter fordelingsoppgave", it)
                         oppgave.opprettFordelingOppgave(jp)
-                        FordelingResultat(jp.journalpostId, "fordelingsoppgave oprettet",MANUELL_FORDELING)
+                        FordelingResultat(journalpostId, "Fordelingsoppgave oprettet",MANUELL_FORDELING)
                     }.getOrElse {
-                        log.warn("Journalpost ${jp.journalpostId} feilet ved opprettelse av manuell fordelingsoppgave")
-                        throw ManuellException(jp.journalpostId,"Feil ved opprettelse av manuell fordelingsoppgave",it)
+                        log.warn("Feil ved opprettelse av manuell fordelingsoppgave for journalpost $journalpostId")
+                        throw ManuellException(journalpostId,"Feil ved opprettelse av manuell fordelingsoppgave",it)
                     }
                 }
             }

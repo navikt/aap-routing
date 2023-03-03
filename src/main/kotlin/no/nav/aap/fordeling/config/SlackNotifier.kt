9020c71d8bcd30e5ae2b42432c7d63b02ccb60a7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-03 16:36:43 +0100

    slack secret

--- src/main/kotlin/no/nav/aap/fordeling/config/SlackNotifier.kt
+++ src/main/kotlin/no/nav/aap/fordeling/config/SlackNotifier.kt
@@ -1,35 +1,38 @@
 package no.nav.aap.fordeling.config
 
-import java.io.IOException
 import com.slack.api.Slack
 import org.slf4j.LoggerFactory
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
 
-class SlackNotifier(private val token: String, private val channel: String, private val enabled: Boolean) {
+@Component
+class SlackNotifier(private val cfg: SlackConfig) {
 
-    init {
-        log.info("Initierer SlackNotifier for kanal: $channel")
-    }
-
-     fun sendMessage(message: String) {
-        if (!enabled) {
-            log.info("Sending av melding via SlackNotifier er ikke skrudd på. Enabled = $enabled")
-            return
-        }
-        try {
-            val response = slack.methods(token).chatPostMessage {
-                it.channel(channel)
-                    .text(message)
+    fun sendMessage(message: String) =
+        with(cfg) {
+            if (enabled) {
+                runCatching {
+                    with(slack.methods(token).chatPostMessage {
+                        it.channel(kanal).text(message)
+                    }) {
+                        if (!isOk) {
+                            log.warn("Klarte ikke sende melding til Slack-kanal: $kanal. Skulle sendt melding: '$message'", errors.joinToString("\n"))
+                        }
+                    }
+                }.getOrElse{
+                    log.warn("Fikk ikke kontakt med Slack sitt API. Skulle ha sendt melding: '$message'", it)
+                }
             }
-            if (!response.isOk) {
-                log.warn("Klarte ikke sende melding til Slack-kanal: $channel. Skulle sendt melding: '$message'", response.errors.joinToString("\n"))
+            else {
+                log.warn("Sending til slack ikke aktivert, set 'slack.enabled: true for aktivering")
             }
-        } catch (e: IOException) {
-            log.warn("Fikk ikke kontakt med Slack sitt API. Skulle ha sendt melding: '$message'", e)
         }
-    }
 
     companion object {
         private val log = LoggerFactory.getLogger(SlackNotifier::class.java)
         private val slack = Slack.getInstance()
     }
 }
+
+@ConfigurationProperties("slack")
+data class SlackConfig(val kanal: String, val token: String, val enabled: Boolean)

931d5f02a6a973e95f338b11a5b69b6eedfa14f3
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-14 09:22:47 +0100

    secure arena flow

--- src/main/kotlin/no/nav/aap/fordeling/arena/ArenaBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arena/ArenaBeanConfig.kt
@@ -1,12 +1,18 @@
 package no.nav.aap.fordeling.arena
 
 import no.nav.aap.fordeling.arena.ArenaConfig.Companion.ARENA
+import no.nav.aap.fordeling.arkiv.dokarkiv.DokarkivConfig
+import no.nav.aap.fordeling.arkiv.dokarkiv.DokarkivConfig.Companion
+import no.nav.aap.fordeling.config.GlobalBeanConfig.Companion.clientCredentialFlow
 import no.nav.aap.health.AbstractPingableHealthIndicator
 import no.nav.boot.conditionals.ConditionalOnGCP
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
+import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders.*
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient.Builder
 
 @Configuration
@@ -14,10 +20,16 @@ class ArenaBeanConfig {
 
     @Bean
     @Qualifier(ARENA)
-    fun arenaWebClient(builder: Builder, cfg: ArenaConfig) =
+    fun arenaWebClient(builder: Builder, @Qualifier(ARENA) arenaFlow: ExchangeFilterFunction, cfg: ArenaConfig) =
         builder
             .baseUrl("${cfg.baseUri}")
+            .filter(arenaFlow)
             .build()
+
+    @Bean
+    @Qualifier(ARENA)
+    fun arenaarkivFlow(cfg: ClientConfigurationProperties, service: OAuth2AccessTokenService) =
+        cfg.clientCredentialFlow(service, ARENA)
 
     @Bean
     @ConditionalOnGCP

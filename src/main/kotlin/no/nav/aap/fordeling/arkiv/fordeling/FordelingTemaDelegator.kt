0660f7191765725223de5ef6d2ddc8ed85e33523
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-15 10:28:54 +0100

    config chore

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/FordelingTemaDelegator.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/FordelingTemaDelegator.kt
@@ -28,7 +28,9 @@ class FordelingTemaDelegator(private val cfg: FordelingConfig, private val forde
 
     fun isEnabled() = cfg.isEnabled
 
-    fun kanFordele(tema: String, status: String) = tema.lowercase() in tema() && status == MOTTATT.name
+    fun kanFordele(tema: String, status: String) = (tema.lowercase() in tema() && status == MOTTATT.name).also {
+        log.info("Kan fordele $tema $status er $it")
+    }
     override fun tema() = fordelere.flatMap { it.tema() }
     override fun fordel(jp: Journalpost, enhet: NAVEnhet) = fordelerFor(jp.tema).fordel(jp, enhet).also {
         metrikker.inc(FORDELING, TEMA,jp.tema, FORDELINGSTYPE, it.fordelingstype.name, KANAL,jp.kanal, BREVKODE,it.brevkode)

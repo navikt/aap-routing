09a56372fbe9743f25be1e2c942a814b4b12338e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-15 18:08:30 +0100

    resilience4j retry listener

--- src/main/kotlin/no/nav/aap/fordeling/config/Resilience4jRetryListener.kt
+++ src/main/kotlin/no/nav/aap/fordeling/config/Resilience4jRetryListener.kt
@@ -6,13 +6,15 @@ import no.nav.aap.fordeling.slack.Slacker
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
-@Component
-class Resilience4jRetryListener(registry: RetryRegistry, slacker: Slacker) {
+@Component class Resilience4jRetryListener(registry: RetryRegistry, slacker: Slacker) {
     private val log = LoggerFactory.getLogger(Resilience4jRetryListener::class.java)
+
     init {
-        registry.retry(GRAPHQL).eventPublisher.onRetry {
-            event -> log.info("Retry event $event")
-            slacker.feilHvisDev("Retry event $event")
+        registry.retry(GRAPHQL).eventPublisher.onRetry { event ->
+            with("Retry event $event") {
+                log.info(this)
+                slacker.feilHvisDev(this)
+            }
         }
     }
 }

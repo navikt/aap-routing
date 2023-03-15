a13b6f0a053f631fc5d43cc569b3a79d38cdc953
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-15 18:18:26 +0100

    log graphql feil

--- src/main/kotlin/no/nav/aap/fordeling/graphql/GraphQLDefaultErrorHandler.kt
+++ src/main/kotlin/no/nav/aap/fordeling/graphql/GraphQLDefaultErrorHandler.kt
@@ -2,15 +2,25 @@ package no.nav.aap.fordeling.graphql
 
 import graphql.kickstart.spring.webclient.boot.GraphQLErrorsException
 import no.nav.aap.fordeling.graphql.GraphQLExtensions.oversett
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class GraphQLDefaultErrorHandler : GraphQLErrorHandler {
 
+    private val log = LoggerFactory.getLogger(GraphQLDefaultErrorHandler::class.java)
+
     override fun handle(e: Throwable): Nothing {
         when (e) {
-            is GraphQLErrorsException -> throw e.oversett()
-            else -> throw e
+            is GraphQLErrorsException ->
+                 e.oversett().also {
+                     log.warn("GraphQL oppslag feilet, håndtert",it)
+                     throw it
+                 }
+            else ->  e.also {
+                log.warn("GraphQL oppslag feilet, ikke håndtert",it)
+                throw it
+            }
         }
     }
 }

85318718ff5df26c07c205a2558a52b8ba8fd1a7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-16 10:25:04 +0100

    mottatt sjekk for å forsikre at ikke andre har journalført endelig i mellomtiden

--- src/main/kotlin/no/nav/aap/fordeling/config/Resilience4jRetryListener.kt
+++ src/main/kotlin/no/nav/aap/fordeling/config/Resilience4jRetryListener.kt
@@ -6,14 +6,35 @@ import no.nav.aap.fordeling.slack.Slacker
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
-@Component class Resilience4jRetryListener(registry: RetryRegistry, slacker: Slacker) {
+@Component
+class Resilience4jRetryListener(registry: RetryRegistry, slacker: Slacker) {
     private val log = LoggerFactory.getLogger(Resilience4jRetryListener::class.java)
 
     init {
-        registry.retry(GRAPHQL).eventPublisher.onRetry { event ->
-            with("Retry event $event") {
-                log.info(this)
-                slacker.feilHvisDev(this)
+        with(registry.retry(GRAPHQL)) {
+            eventPublisher.onError {
+                with("Retry event error $it") {
+                    log.warn(this,it.lastThrowable)
+                    slacker.feilHvisDev(this)
+                }
+            }
+            eventPublisher.onSuccess {
+                with("Retry event success $it") {
+                    log.info(this)
+                    slacker.okHvisDev(this)
+                }
+            }
+            eventPublisher.onIgnoredError() {
+                with("Retry event ignore $it") {
+                    log.warn(this,it.lastThrowable)
+                    slacker.feilHvisDev(this)
+                }
+            }
+            registry.retry(GRAPHQL).eventPublisher.onRetry {
+                with("Retry event $it") {
+                    log.warn(this,it.lastThrowable)
+                    slacker.feilHvisDev(this)
+                }
             }
         }
     }

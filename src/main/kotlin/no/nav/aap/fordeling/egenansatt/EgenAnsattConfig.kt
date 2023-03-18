4399931712100755f41592cfaf991c4ab8cb9bf2
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-18 20:11:48 +0100

    metrics for retry

--- src/main/kotlin/no/nav/aap/fordeling/egenansatt/EgenAnsattConfig.kt
+++ src/main/kotlin/no/nav/aap/fordeling/egenansatt/EgenAnsattConfig.kt
@@ -1,6 +1,8 @@
 package no.nav.aap.fordeling.egenansatt
 
 import java.net.URI
+import java.time.Duration
+import java.time.Duration.ofSeconds
 import no.nav.aap.fordeling.egenansatt.EgenAnsattConfig.Companion.EGENANSATT
 import no.nav.aap.rest.AbstractRestConfig
 import no.nav.aap.rest.AbstractRestConfig.RetryConfig.Companion.DEFAULT
@@ -13,7 +15,7 @@ class EgenAnsattConfig(
         @DefaultValue(DEFAULT_PING_PATH) pingPath: String,
         @DefaultValue("true") enabled: Boolean,
         @DefaultValue(SKJERMING_PATH) val path: String,
-        baseUri: URI) : AbstractRestConfig(baseUri, pingPath, EGENANSATT, enabled, DEFAULT) {
+        baseUri: URI) : AbstractRestConfig(baseUri, pingPath, EGENANSATT, enabled, RetryConfig(1L,ofSeconds(1))) {
 
     fun skjermetUri(b: UriBuilder) = b.path(path).build()
     override fun toString() = "${javaClass.simpleName} [pingPath=$pingPath,enabled=$isEnabled,baseUri=$baseUri]"

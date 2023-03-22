d9b4305444789cd1f69a788339e47cb328962967
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-22 19:41:18 +0100

    prøv igjen med logging

--- src/main/kotlin/no/nav/aap/fordeling/navenhet/NavEnhetBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/fordeling/navenhet/NavEnhetBeanConfig.kt
@@ -25,9 +25,9 @@ class NavEnhetBeanConfig(private val mapper: ObjectMapper) {
     fun navEnhetWebClient(builder: Builder, cfg: NavEnhetConfig) =
         builder
             .baseUrl("${cfg.baseUri}")
-            .codecs { codec ->
-                codec.defaultCodecs().jackson2JsonDecoder(LoggingJsonDecoder {
-                   log.info("Response er ${mapper.readValue<List<Any>>(it)}")
+            .codecs { c ->
+                c.defaultCodecs().jackson2JsonDecoder(LoggingJsonDecoder {
+                   log.info("Response er ${mapper.readValue<List<Map<String,Any>>>(it)}")
                 })
             }
             .build()

113b93cdce9ed67188f3b5038fb24a526f37f62f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-23 08:12:33 +0100

    log

--- src/main/kotlin/no/nav/aap/fordeling/navenhet/NavEnhetWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/fordeling/navenhet/NavEnhetWebClientAdapter.kt
@@ -39,7 +39,7 @@ class NavEnhetWebClientAdapter(@Qualifier(NAVENHET) webClient: WebClient, val cf
         .accept(APPLICATION_JSON)
         .exchangeToMono { it.toResponse<List<NavOrg>>(log)}
         .retryWhen(cf.retrySpec(log,cf.aktive))
-        .doOnSuccess { log.info("Aktive enheter oppslag  NORG2 OK. Respons ga ${it.size} innslag") }
+        .doOnSuccess { log.info("Aktive enheter oppslag  NORG2 OK. Respons ga ${it.size} innslag ${it.first()}") }
         .doOnError { t -> log.warn("Aktive enheter oppslag feilet", t) }
         .block() ?: throw IrrecoverableIntegrationException("Kunne ikke hente aktive enheter")
 

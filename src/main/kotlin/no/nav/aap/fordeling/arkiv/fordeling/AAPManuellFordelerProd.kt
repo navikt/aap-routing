69e9d580836b47ddffcd9d5405b88d38ce70c81d
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-24 23:03:26 +0100

    toString + bump

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/AAPManuellFordelerProd.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/AAPManuellFordelerProd.kt
@@ -6,6 +6,7 @@ import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.FordelingResultat.Ford
 import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.FordelingResultat.FordelingType.MANUELL_JOURNALFØRING
 import no.nav.aap.fordeling.navenhet.EnhetsKriteria.NavOrg.NAVEnhet
 import no.nav.aap.fordeling.oppgave.OppgaveClient
+import no.nav.aap.util.Constants.AAP
 import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.boot.conditionals.Cluster
 import no.nav.boot.conditionals.Cluster.Companion
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Component
 class AAPManuellFordelerProd(private val oppgave: OppgaveClient) : AAPManuellFordeler(oppgave) {
     override val log = getLogger(AAPManuellFordelerProd::class.java)
 
+    override fun tema() = listOf(AAP)
     override fun clusters() = prodClusters()
 
     override fun opprettFordeling(jp: Journalpost) = log.info("Liksom oppretter fordelingsoppgave")

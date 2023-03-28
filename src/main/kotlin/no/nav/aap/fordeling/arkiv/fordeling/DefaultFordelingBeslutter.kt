3216de40f61c0600b0e5e2ee98fc4c0c7b5c376c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-28 11:15:44 +0200

    ignorer visse kanaler

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/DefaultFordelingBeslutter.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/DefaultFordelingBeslutter.kt
@@ -1,14 +1,25 @@
 package no.nav.aap.fordeling.arkiv.fordeling
 
 import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.JournalpostDTO.JournalStatus.MOTTATT
+import okhttp3.internal.ignoreIoExceptions
 import org.springframework.stereotype.Component
 
 @Component
 class DefaultFordelingBeslutter(private val cfg: FordelingConfig): FordelingBeslutter {
-    override fun skalFordele(jp: Journalpost) = cfg.isEnabled && jp.status == MOTTATT  && !jp.erMeldekort()
+    override fun skalFordele(jp: Journalpost) = cfg.isEnabled
+            && jp.status == MOTTATT
+            && !jp.erMeldekort()
+            && jp.kanal !in IGNORERTE_KANALER
+
+    companion object {
+        private const val EESSI = "EESSI"
+        private const val NAV_NO_CHAT = "NAV_NO_CHAT"
+        private val IGNORERTE_KANALER = listOf(EESSI, NAV_NO_CHAT)
+    }
 }
 
 interface FordelingBeslutter {
+    
     fun skalFordele(jp:Journalpost) : Boolean
 
 }

dc23b59e465e144072f92df0ddc327b1d7341faf
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-29 08:51:39 +0200

    saf monkey

--- src/main/kotlin/no/nav/aap/fordeling/graphql/GraphQLRetryListener.kt
+++ src/main/kotlin/no/nav/aap/fordeling/graphql/GraphQLRetryListener.kt
@@ -2,43 +2,43 @@ package no.nav.aap.fordeling.graphql
 
 import io.github.resilience4j.retry.RetryRegistry
 import io.github.resilience4j.springboot3.retry.autoconfigure.RetryProperties
-import jakarta.annotation.PostConstruct
 import no.nav.aap.fordeling.slack.Slacker
 import org.slf4j.LoggerFactory
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 
 @Component
-class GraphQLRetryListener(private val registry: RetryRegistry, private val slacker: Slacker, private val p: RetryProperties) {
+class GraphQLRetryListener(private val registry: RetryRegistry, private val slacker: Slacker, private val p: RetryProperties) : ApplicationListener<ApplicationReadyEvent> {
     private val log = LoggerFactory.getLogger(GraphQLRetryListener::class.java)
-
-    @PostConstruct
-    fun init() = p.instances.keys.forEach { registerRetryEventListeners(it) }
-
+    override fun onApplicationEvent(event: ApplicationReadyEvent)= p.instances.keys.forEach { registerRetryEventListeners(it) }
     private fun registerRetryEventListeners(key: String) {
         log.info("Registrerer retry listener for ${key.uppercase()}")
         with(registry.retry(key)) {
-            eventPublisher.onError {
-                with("Retry event error $it") {
-                    log.warn(this, it.lastThrowable)
-                    slacker.feilHvisDev(this)
+            with(eventPublisher) {
+                onError {
+                    with("Retry event error $it") {
+                        log.warn(this, it.lastThrowable)
+                        slacker.feilHvisDev(this)
+                    }
                 }
-            }
-            eventPublisher.onSuccess {
-                with("Retry event success ${key.uppercase()} $it") {
-                    log.info(this)
-                    slacker.okHvisDev(this)
+                onSuccess {
+                    with("Vellykket retry på forsøk ${it.numberOfRetryAttempts} for '${it.name.uppercase()}' etter exception ${it.lastThrowable}") {
+                        log.info(this)
+                        slacker.okHvisDev(this)
+                    }
                 }
-            }
-            eventPublisher.onIgnoredError() {
-                with("Retry event ignore ${key.uppercase()} $it") {
-                    log.warn(this, it.lastThrowable)
-                    slacker.feilHvisDev(this)
+                onIgnoredError() {
+                    with("Ingen retry for ${it.lastThrowable.javaClass.name}  for '${it.name.uppercase()}'") {
+                        log.warn(this, it.lastThrowable)
+                        slacker.feilHvisDev(this)
+                    }
                 }
-            }
-            eventPublisher.onRetry {
-                with("Retry event ${key.uppercase()} $it") {
-                    log.warn(this, it.lastThrowable)
-                    slacker.feilHvisDev(this)
+                onRetry {
+                    with("Gjør retry for ${it.numberOfRetryAttempts}. gang pga. ${it.lastThrowable.javaClass.name} for '${it.name.uppercase()}'") {
+                        log.warn(this, it.lastThrowable)
+                        slacker.feilHvisDev(this)
+                    }
                 }
             }
         }

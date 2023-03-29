ed06b9bb5fb47d618dd5616fa3f8c97a1069dc83
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-29 08:21:48 +0200

    saf monkey

--- src/main/kotlin/no/nav/aap/fordeling/graphql/GraphQLRetryListener.kt
+++ src/main/kotlin/no/nav/aap/fordeling/graphql/GraphQLRetryListener.kt
@@ -1,39 +1,46 @@
 package no.nav.aap.fordeling.graphql
 
 import io.github.resilience4j.retry.RetryRegistry
+import no.nav.aap.fordeling.arkiv.saf.SAFConfig.Companion.SAF
 import no.nav.aap.fordeling.graphql.AbstractGraphQLAdapter.Companion.GRAPHQL
+import no.nav.aap.fordeling.person.PDLConfig.Companion.PDL
 import no.nav.aap.fordeling.slack.Slacker
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
-class GraphQLRetryListener(registry: RetryRegistry, slacker: Slacker) {
+class GraphQLRetryListener(private val registry: RetryRegistry, private val slacker: Slacker) {
     private val log = LoggerFactory.getLogger(GraphQLRetryListener::class.java)
 
     init {
-        log.info("Registrerer retry listener")
-        with(registry.retry(GRAPHQL)) {
+        register(SAF)
+        register(PDL)
+    }
+
+    private fun GraphQLRetryListener.register(cfg: String) {
+        log.info("Registrerer retry listener for $cfg")
+        with(registry.retry(SAF)) {
             eventPublisher.onError {
                 with("Retry event error $it") {
-                    log.warn(this,it.lastThrowable)
+                    log.warn(this, it.lastThrowable)
                     slacker.feilHvisDev(this)
                 }
             }
             eventPublisher.onSuccess {
-                with("Retry event success $it") {
+                with("Retry event success ${cfg.uppercase()} $it") {
                     log.info(this)
                     slacker.okHvisDev(this)
                 }
             }
             eventPublisher.onIgnoredError() {
-                with("Retry event ignore $it") {
-                    log.warn(this,it.lastThrowable)
+                with("Retry event ignore ${cfg.uppercase()} $it") {
+                    log.warn(this, it.lastThrowable)
                     slacker.feilHvisDev(this)
                 }
             }
             registry.retry(GRAPHQL).eventPublisher.onRetry {
-                with("Retry event $it") {
-                    log.warn(this,it.lastThrowable)
+                with("Retry event ${cfg.uppercase()} $it") {
+                    log.warn(this, it.lastThrowable)
                     slacker.feilHvisDev(this)
                 }
             }

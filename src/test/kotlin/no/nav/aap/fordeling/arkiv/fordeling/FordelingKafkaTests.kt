af2c70f9f12076e8ae23acb9065d349f7b3c19cd
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-31 12:20:09 +0200

    test ettersending

--- src/test/kotlin/no/nav/aap/fordeling/arkiv/fordeling/FordelingKafkaTests.kt
+++ src/test/kotlin/no/nav/aap/fordeling/arkiv/fordeling/FordelingKafkaTests.kt
@@ -1,3 +1,5 @@
+package no.nav.aap.fordeling.arkiv.fordeling
+
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -5,14 +7,11 @@ import org.springframework.kafka.test.EmbeddedKafkaBroker
 import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import no.nav.aap.fordeling.arkiv.fordeling.FordelingBeslutter
-import no.nav.aap.fordeling.arkiv.fordeling.FordelingConfig
 
 @EmbeddedKafka
 @ExtendWith(SpringExtension::class)
 @ContextConfiguration(classes = [FordelingBeslutter::class, FordelingConfig::class])
-//@SpringJUnitConfig(classes = [MottattAwareFordelingBeslutter::class])
-public class KafkaTests {
+public class FordelingKafkaTests {
 
     @Autowired
     lateinit var broker : EmbeddedKafkaBroker

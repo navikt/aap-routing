5ccf2266f55e2a7bf12fbf0c6f56dea1b4238c8c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-31 18:00:20 +0200

    fix test

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/FordelingBeslutter.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/FordelingBeslutter.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.fordeling.arkiv.fordeling
 
 import org.springframework.stereotype.Component
+import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.JournalpostDTO.JournalStatus.MOTTATT
 import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.JournalpostDTO.Kanal.EESSI
 import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.JournalpostDTO.Kanal.EKST_OPPS
 import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.JournalpostDTO.Kanal.NAV_NO_CHAT
@@ -11,6 +12,7 @@ class FordelingBeslutter(private val cfg : FordelingConfig = FordelingConfig()) 
     fun skalFordele(jp : Journalpost) =
         with(jp) {
             cfg.isEnabled
+                && status == MOTTATT
                 && !erMeldekort()
                 && kanal !in HÅNDTERES_AV_ANDRE
         }
--- src/test/kotlin/no/nav/aap/fordeling/arkiv/fordeling/TestFordelingBeslutter.kt
+++ src/test/kotlin/no/nav/aap/fordeling/arkiv/fordeling/TestFordelingBeslutter.kt
@@ -2,9 +2,11 @@ package no.nav.aap.fordeling.arkiv.fordeling
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.JournalpostDTO.JournalStatus.JOURNALFOERT
 import no.nav.aap.fordeling.arkiv.fordeling.FordelingDTOs.JournalpostDTO.Kanal.EESSI
 import no.nav.aap.fordeling.arkiv.fordeling.TestData.JP
 import no.nav.aap.fordeling.arkiv.fordeling.TestData.medKanal
+import no.nav.aap.fordeling.arkiv.fordeling.TestData.medStatus
 import no.nav.aap.fordeling.arkiv.fordeling.TestData.somMeldekort
 
 class TestFordelingBeslutter {
@@ -13,6 +15,7 @@ class TestFordelingBeslutter {
     fun testBeslutter() {
         with(FordelingBeslutter()) {
             assertThat(skalFordele(JP)).isTrue()
+            assertThat(skalFordele(JP.medStatus(JOURNALFOERT))).isFalse()
             assertThat(skalFordele(JP.medKanal(EESSI))).isFalse()
             assertThat(skalFordele(JP.somMeldekort())).isFalse()
         }

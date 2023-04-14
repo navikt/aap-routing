fcb5f1a600e48e1f95fc3735888b483bd9dfdf4f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-04-14 20:37:59 +0200

    ql

--- src/main/kotlin/no/nav/aap/fordeling/graphql/BootGraphQLDefaultErrorHandler.kt
+++ src/main/kotlin/no/nav/aap/fordeling/graphql/BootGraphQLDefaultErrorHandler.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import org.springframework.graphql.client.GraphQlClientException
 import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import no.nav.aap.api.felles.error.IrrecoverableIntegrationException
 import no.nav.aap.fordeling.graphql.GraphQLExtensions.IrrecoverableGraphQLException.UnexpectedResponseGraphQLException
 import no.nav.aap.fordeling.graphql.GraphQLExtensions.RecoverableGraphQLException.UnhandledGraphQLException
 
@@ -12,7 +13,7 @@ class BootGraphQLDefaultErrorHandler : GraphQLErrorHandler {
     override fun handle(e : Throwable, query : String) : Nothing {
         when (e) {
             is MismatchedInputException -> throw UnexpectedResponseGraphQLException(BAD_REQUEST, "Ikke-håndtert respons for  $query")
-            is GraphQlClientException -> throw e
+            is GraphQlClientException -> throw IrrecoverableIntegrationException(e.message, cause = e)
             else -> throw UnhandledGraphQLException(INTERNAL_SERVER_ERROR, "GraphQL oppslag $query feilet", e)
         }
     }

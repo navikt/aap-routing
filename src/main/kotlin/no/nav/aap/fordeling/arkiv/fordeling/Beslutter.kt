23d564222281e9cf0b4a6d91d05bfb380a77226b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-04-27 18:54:41 +0200

    no proxy

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/Beslutter.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/fordeling/Beslutter.kt
@@ -14,37 +14,3 @@ class ArenaBeslutter : Beslutter {
 
     override fun beslutt(jp : Journalpost) = ARENA
 }
-
-/*
-class HoveddokumentInspiserendeBeslutter(val arkiv : ArkivClient, private val mapper : ObjectMapper) : InspisererendeBeslutter {
-
-
-    private val log = LoggerUtil.getLogger(DefaltInspiserendeBeslutter::class.java)
-
-    override fun beslutt(jp : Journalpost) : FordelingsBeslutning {
-        return runCatching {
-            if (jp.harOriginal()) {
-                arkiv.hentSøknad(jp).also {
-                    it?.let {
-                        if (gyldigJson(it)) {
-                            log.info("Søknad er gyldig JSON")
-                        }
-                    }
-                }
-                TIL_ARENA
-            }
-        }.getOrElse {
-            TIL_ARENA as FordelingsBeslutning
-        }
-    }
-
-    private fun gyldigJson(json : String) =
-        runCatching {
-            mapper.readTree(json)
-            true
-        }.getOrElse {
-            log.warn("Ugyldig json", it)
-            false
-        }
-}
- */

f7b8a4d5fb43c3ccb4b278d43ea24bb7465d4856
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-05 16:17:12 +0200

    fjernet gammel graphql-adaper spor

--- src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivClient.kt
+++ src/main/kotlin/no/nav/aap/fordeling/arkiv/ArkivClient.kt
@@ -6,27 +6,15 @@ import no.nav.aap.fordeling.arkiv.dokarkiv.DokarkivWebClientAdapter
 import no.nav.aap.fordeling.arkiv.fordeling.Journalpost
 import no.nav.aap.fordeling.arkiv.saf.SAFGraphQLAdapter
 import no.nav.aap.fordeling.arkiv.saf.SAFWebClientAdapter
-import no.nav.aap.util.LoggerUtil
 
 @Component
 @Observed(contextualName = "Arkiv")
 class ArkivClient(private val dokarkiv : DokarkivWebClientAdapter, private val safdok : SAFWebClientAdapter, private val saf : SAFGraphQLAdapter) {
 
-    val log = LoggerUtil.getLogger(ArkivClient::class.java)
-
     fun hentSøknad(jp : Journalpost) = safdok.originalDokument(jp)
 
-    fun hentJournalpost(jp : String) =// runCatching {
-        saf.hentJournalpost1(jp).also {
-            log.info("spring graphql jp ok")
-        }
+    fun hentJournalpost(jp : String) = saf.hentJournalpost1(jp)
 
-    /*
-    }.getOrElse {
-        log.warn("spring graphql jp feil", it)
-        saf.hentJournalpost(jp)
-    }
-*/
     fun oppdaterOgFerdigstillJournalpost(jp : Journalpost, sakNr : String) =
         dokarkiv.oppdaterOgFerdigstillJournalpost(jp, sakNr)
 
--- src/main/kotlin/no/nav/aap/fordeling/person/PDLClient.kt
+++ src/main/kotlin/no/nav/aap/fordeling/person/PDLClient.kt
@@ -1,7 +1,6 @@
 package no.nav.aap.fordeling.person
 
 import io.micrometer.observation.annotation.Observed
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import no.nav.aap.api.felles.AktørId
 import no.nav.aap.api.felles.Fødselsnummer
@@ -10,19 +9,9 @@ import no.nav.aap.api.felles.Fødselsnummer
 @Observed(contextualName = "PDL")
 class PDLClient(private val a : PDLWebClientAdapter) {
 
-    private val log = LoggerFactory.getLogger(PDLClient::class.java)
-    fun geoTilknytning(fnr : Fødselsnummer) = //runCatching {
-        a.geoTilknytning(fnr).also {
-            log.info("spring graphql geo ok")
-        }
+    fun geoTilknytning(fnr : Fødselsnummer) = a.geoTilknytning(fnr)
 
-    fun diskresjonskode(fnr : Fødselsnummer) = // runCatching {
-        a.diskresjonskode(fnr).also {
-            log.info("spring graphql diskresjon ok")
-        }
+    fun diskresjonskode(fnr : Fødselsnummer) = a.diskresjonskode(fnr)
 
-    fun fnr(id : AktørId) = //runCatching {
-        a.fnr(id).also {
-            log.info("spring graphql fnr ok")
-        }
+    fun fnr(id : AktørId) = a.fnr(id)
 }

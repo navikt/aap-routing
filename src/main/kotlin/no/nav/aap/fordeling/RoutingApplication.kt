6bbff74ea827746b4558b7cc16cb1e84c6e9086d
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-06-02 21:56:36 +0200

    remove observed

--- src/main/kotlin/no/nav/aap/fordeling/RoutingApplication.kt
+++ src/main/kotlin/no/nav/aap/fordeling/RoutingApplication.kt
@@ -3,7 +3,6 @@ package no.nav.aap.fordeling
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import no.nav.aap.util.AccessorUtil
 import no.nav.boot.conditionals.Cluster.Companion.profiler
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
@@ -16,7 +15,7 @@ class RoutingApplication
 
 fun main(args : Array<String>) {
     runApplication<RoutingApplication>(*args) {
-        AccessorUtil.init()
+        // AccessorUtil.init()
         setAdditionalProfiles(*profiler())
     }
 }
